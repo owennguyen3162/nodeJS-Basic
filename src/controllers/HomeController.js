75300a521df25214ef7dc791f02599874577cf3f
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-29 22:13:16 +0700

    tutorial-day06
    ROUTER PARAMS - DETAIL - EDIT

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,9 +1,38 @@
 const connection = require("../config/connectDB");
 
-const getHomePage = (req, res) => {
-  connection.query("SELECT * FROM users", (err, results, fields) =>
-    res.render("index.ejs", { dataUser: results })
-  );
+const getHomePage = async (req, res) => {
+  const conn = await connection;
+  const query = "SELECT * FROM users";
+  try {
+    const [data] = await conn.execute(query);
+    return res.render("index.ejs", { dataUser: data });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = { getHomePage };
+const getUserDetail = async (req, res) => {
+  const userId = await req.params.id;
+  const conn = await connection;
+  const query = `SELECT * FROM users where id = ?`;
+  try {
+    const [data] = await conn.execute(query, [userId]);
+    return res.render("userDetail.ejs", { UserInfo: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getEditUser = async (req, res) => {
+  const userId = await req.params.id;
+  const conn = await connection;
+  const query = `SELECT * FROM users where id = ?`;
+  try {
+    const [data] = await conn.execute(query, [userId]);
+    return res.render("editUser.ejs", { UserInfo: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { getHomePage, getUserDetail, getEditUser };
