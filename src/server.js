dbec8d84b698848b34c214cf1967d401f2d5e69c
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-29 22:13:38 +0700

    tutorial-day06
    ROUTER PARAMS - DETAIL - EDIT

--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ const configViewEngine = require("./config/configView"); //config ViewEngine
 require("dotenv").config();
 const app = express(); // sử dụng các tính năng của thằng express
 const PORT = process.env.PORT || 3030;
+const methodOverride = require('method-override')
 const route = require("./route");
 
+app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 configViewEngine(app); //setup view engine
