3faf29b9176b0383aa607bd1d0d8402b3836cfe2
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-29 20:09:53 +0700

    tutorial-day05
    CREATE NAV - EJS LOOP DATA

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,7 +2,7 @@ const connection = require("../config/connectDB");
 
 const getHomePage = (req, res) => {
   connection.query("SELECT * FROM users", (err, results, fields) =>
-    res.render("index.ejs", { dataUser: JSON.stringify(results) })
+    res.render("index.ejs", { dataUser: results })
   );
 };
 
