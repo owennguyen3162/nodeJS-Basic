35f79c0141fb3bfcc70eba795200f3fc2de355df
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-29 14:44:19 +0700

    tutorial-day01

---
+++ src/config/configView.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const configViewEngine = (app) =>{
+    app.set("view engine","ejs");
+    app.set("views","./src/views");
+}
+
+module.exports = configViewEngine;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const configViewEngine = require("./config/configView"); //config ViewEngine
 
 const app = express(); // sử dụng các tính năng của thằng express
 const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+configViewEngine(app);
 
-app.get("/",(req,res) => {
-    res.send("HELLO")
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
 app.listen(PORT, () => console.log("Listening port " + PORT));
