b5e220874e4947bd6abc5bb53b73f3784b62cefc
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-29 22:13:28 +0700

    tutorial-day06
    ROUTER PARAMS - DETAIL - EDIT

--- src/route/Home.router.js
+++ src/route/Home.router.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const HomeController = require("../controllers/HomeController");
 
+router.get("/user/detail/:id", HomeController.getUserDetail);
+router.get("/user/edit/:id", HomeController.getEditUser);
 router.get("/", HomeController.getHomePage);
 
 module.exports = router;
