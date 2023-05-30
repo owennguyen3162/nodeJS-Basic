ce77c4ca9952022a70f31a4034d1ea2e83708c94
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-30 16:22:15 +0700

    tutorial-day09
    RESTFULL API WITH NOTEJS

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const home = require("./Home.router");
+const api = require("./Api.router");
 
 const route = (app) => {
+  app.use("/api", api);
   app.use("/", home);
 };
 
