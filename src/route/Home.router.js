32b66986be77e477279db56050219d68bca82fa8
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-30 13:46:02 +0700

    tutorial-day08
    COMPLETE CRUD - ADD, DISPLAY, UPDATE, AND DELETE DATA | Basic Node.js

--- src/route/Home.router.js
+++ src/route/Home.router.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const HomeController = require("../controllers/HomeController");
 
 router.post("/user/addUser/store", HomeController.addNewUser);
+router.put("/user/edit/:userId", HomeController.editUser);
+router.delete("/user/delete/:userId", HomeController.deleteUser);
 
 router.get("/user/detail/:id", HomeController.getUserDetail);
 router.get("/user/edit/:id", HomeController.getEditUser);
